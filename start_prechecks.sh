a19044d2e4ee03063010a0324f350f720b673fef
dasanirban834 <67305577+dasanirban834@users.noreply.github.com>
2022-06-27 21:19:24 +0530

    Delete start_prechecks.sh

--- start_prechecks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-server_list=$(jq . start.json | jq .Servers)
-for j in ${#server_list[@]}
-do
-        aws ec2 describe-instances --instance-id $(server_list[i]) --query "Reservations[*].Instances[*].{PrivateIP:PrivateIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}" --output table
-        j=j+1
-done
